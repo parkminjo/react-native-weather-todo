d0c34a47f402cc91d4b2a4119111aad07d5784ac
parkminjo <mj.park@sigcrew.com>
2026-01-06 15:29:08 +0900

    init: 초기 세팅

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>text</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({ container: { flex: 1 } });
